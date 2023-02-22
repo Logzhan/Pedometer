79b1132b808674d81a4ef4694aa8465c09085830
logzhan <719901725@qq.com>
2023-02-22 22:38:31 +0800

    调通MotionAlgorithm在Android JNI的集成

--- 1.Application/MotionAlgorithm/app/src/main/java/com/geek/motion/MainActivity.kt
+++ 1.Application/MotionAlgorithm/app/src/main/java/com/geek/motion/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.geek.motion
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(),SensorEventListener{
+    private var sensorManager: SensorManager? = null
+    private var sensor: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSensor()
         textView.text = stringFromJNI()
+
+        pedometerInit()
     }
+
+    private fun initSensor() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager?.registerListener(this, sensor, 100)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+    @SuppressLint("SetTextI18n")
+    override fun onSensorChanged(event: SensorEvent?) {
+        val values = event?.values
+        val x = values!![0]
+        val y = values[1]
+        val z = values[2]
+        val step = pedometerUpdate(x, y, z)
+        val str = "Step = $step"
+        textView?.text = str
+    }
+
     private external fun stringFromJNI(): String
+    private external fun pedometerInit()
+    private external fun pedometerUpdate(x:Float, y:Float, z:Float): Long
     companion object {
         // Used to load the 'native-lib' library on application startup.
         init {
--- 1.Application/MotionAlgorithm/app/src/main/java/jni/native-lib.cpp
+++ 1.Application/MotionAlgorithm/app/src/main/java/jni/native-lib.cpp
@@ -1,10 +1,30 @@
 #include <jni.h>
 #include <string>
-
+#include "Pedometer.h"
 extern "C"
+
 JNIEXPORT jstring JNICALL
 Java_com_geek_motion_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     // TODO: implement stringFromJNI()
-    std::string test = "main ++++";
+
+    std::string test = std::string(Pedometer_GetVersion());
     return env->NewStringUTF(test.c_str());
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_geek_motion_MainActivity_pedometerUpdate(JNIEnv *env, jobject thiz, jfloat x, jfloat y,
+                                                    jfloat z) {
+    // TODO: implement Pedometer_Update()
+    float s  = 1000.0f;
+    int ax = (int)(x * s);
+    int ay = (int)(y * s);
+    int az = (int)(z * s);
+    return Pedometer_Update(ax, ay, az);
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_geek_motion_MainActivity_pedometerInit(JNIEnv *env, jobject thiz) {
+    // TODO: implement pedometerInit()
+    Pedometer_Init();
 }
